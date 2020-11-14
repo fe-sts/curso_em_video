3c7d3a1bf08850f1d041639163b2b329baa071f8
fe-sts <felipe.rinaldini@gmail.com>
2020-11-14 18:45:26 -0300

    Aula 004 / Exercicios de 012

---
+++ Lista de Exercicios/Exercicio 004 (012).py
@@ -0,0 +1,5 @@
+#Faça um algoritmo que leia o preço de um produto  e mostre  seu novo preço, com 5% de desconto.
+
+preco = float(input('Entre com o preço do produto (Ex.: 10.50):'))
+
+print('O preço com desconto de 5% é {}'.format(preco*0.95))
