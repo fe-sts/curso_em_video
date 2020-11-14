84e3e821af7d3c0eb7a3f0391c272cb2419c6d8f
fe-sts <felipe.rinaldini@gmail.com>
2020-11-14 18:55:09 -0300

    Aula 004 / Exercicios de 013

---
+++ Lista de Exercicios/Exercicio 004 (013).py
@@ -0,0 +1,6 @@
+# Escreva um algoritmo que leia o salario de um funcionario e mostre seu novo  salário, com 15% de aumento
+
+sal = float(input('Entre com o seu salário (Ex.: 1500.50): '))
+
+print('Seu novo salário, com 15% de aumento é {}'.format(sal*1.15))
+
