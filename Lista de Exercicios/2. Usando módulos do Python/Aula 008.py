170b5864cc47d237f3dfe886432357d4eb0d5099
fe-sts <felipe.rinaldini@gmail.com>
2020-12-02 00:37:11 -0300

    Aula 008

---
+++ Lista de Exercicios/2. Usando módulos do Python/Aula 008.py
@@ -0,0 +1,12 @@
+#from math import sqrt
+#num = int(input('Digite um numero: '))
+#raiz = sqrt(num)
+#print('A raiz quadrada de {} é: {:.2f}'.format(num, raiz))
+
+
+#import random
+#num = random.randint(1, 10)
+#print(num)
+
+#import emoji
+#print(emoji.emojize('Olá, mundo :sunglasses:', use_alises=True))
