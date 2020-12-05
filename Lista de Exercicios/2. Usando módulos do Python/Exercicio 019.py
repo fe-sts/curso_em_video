97168e542f297b8ac3e55fa60cf79a7520a5aa6b
fe-sts <felipe.rinaldini@gmail.com>
2020-12-05 01:25:47 -0300

    Exercicio 019.py: for / for lista.append / random.choice

---
+++ Lista de Exercicios/2. Usando módulos do Python/Exercicio 019.py
@@ -0,0 +1,10 @@
+#Um professor quer sortear um dos seus 4 alunos para apagar o quadro.
+#Faça um programa que ajude ele, lendo o nome deles
+import random
+n_aluno = int(1)
+alunos = []
+for a in range(4):
+    alunos.append(input('Digite o nome do aluno {} :'.format(n_aluno)))
+    n_aluno += 1
+#alunos.extend(input('Digite o nome dos alunos separados por virgula (,): ')) #nao funciona pois cada letra vira uma instância dentro da lista
+print('{}'.format(random.choice(alunos)))
