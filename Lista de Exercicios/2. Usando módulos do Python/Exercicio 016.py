db8dfe45557cdc533fe78883f3b5ae28fffecb6f
fe-sts <felipe.rinaldini@gmail.com>
2020-12-04 21:56:18 -0300

    Exercicio 016.py

---
+++ Lista de Exercicios/2. Usando módulos do Python/Exercicio 016.py
@@ -0,0 +1,8 @@
+#Crie um programa que leia um numero Real qualquer pelo teclado e mostre na tela a sua porção Inteira
+#Ex.: 6,16 --> 6
+
+import math
+n1 = float(input('Digite um numero: '))
+inteiro = math.trunc(n1) #truncado
+print('Para o numero inserido {}, sua porção Inteira é: {}'.format(n1, inteiro))
+print('Outro metodo para ficar Inteiro {:.0f}'.format(n1))
