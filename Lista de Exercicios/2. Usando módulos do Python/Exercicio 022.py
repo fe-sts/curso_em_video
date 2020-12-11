0ce57767ff9cfead8961e3a2f77e69542b4d6e53
fe-sts <felipe.rinaldini@gmail.com>
2020-12-11 00:27:29 -0300

    Exercicio 022.py: upper/lower/strip/split/join

---
+++ Lista de Exercicios/2. Usando módulos do Python/Exercicio 022.py
@@ -0,0 +1,12 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# 1. O nome com todas as letras maiusculas
+# 2. O nome com todas as letras minusculas
+# 3. Quantas letras ao to do (sem considerar espaços)
+# 4. Quantas letras tem o 1o nome
+
+nome = str(input('Digite seu nome completo: '))
+
+print('Seu nome UPPER: {}'.format(nome.upper()))
+print('Seu nome lower: {}'.format(nome.lower()))
+print('Nº de letras sem espaços (começo e final): {}'.format(len(nome.strip())))
+print('Nº de letras sem espaços (começo, final e meio): {}'.format(len("".join(nome.split()))))
