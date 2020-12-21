47eba6524b3ccfa3997c88449e5e53341ffa9299
fe-sts <felipe.rinaldini@gmail.com>
2020-12-21 00:25:59 -0300

    Exercicio 022: subtraindo com metodos| split string para lista

--- Lista de Exercicios/2. Usando módulos do Python/Exercicio 022.py
+++ Lista de Exercicios/2. Usando módulos do Python/Exercicio 022.py
@@ -5,10 +5,20 @@
 # 4. Quantas letras tem o 1o nome
 
 nome = str(input('Digite seu nome completo: '))
-
+#1
 print('Seu nome UPPER: {}'.format(nome.upper()))
+
+#2
 print('Seu nome lower: {}'.format(nome.lower()))
+
+#3
 print('Nº de letras sem espaços (começo e final): {}'.format(len(nome.strip())))
 print('Nº de letras sem espaços (começo, final e meio): {}'.format(len("".join(nome.split()))))
+print('Nº de letras sem espaços (começo, final e meio)(Outro jeito): {}'.format(len(nome) - nome.count(' ')))
+
+#4
 nome1 = nome[:nome.find(' ')]
-print('Quantas letras tem o 1o nome {}'.format(nome1))
+print('Quantas letras tem o 1o nome {}'.format(len(nome1)))
+print('Quantas letras tem o 1o nome {} (Outro jeito)'.format(nome.find(' ')))
+separado_1 = nome.split()
+print('Quantas letras tem o 1o nome {} (Jeito 3)'. format(separado_1[0]))
