a7cbc176a5a316df14a5147f8f6328b53a36560a
fe-sts <felipe.rinaldini@gmail.com>
2020-12-22 00:31:29 -0300

    Exercicio 030: estrutura de decisão if| resto da divisão com %

---
+++ Lista de Exercicios/3.  Condições em Python (if..else)/Exercicio 030.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia um numero interio qualquer e diga de ele é par ou impar
+
+num = int(input('Digite um numero: '))
+if (num % 2) == 0:
+    print('Numero Par')
+else:
+    print('Numero Impar')
