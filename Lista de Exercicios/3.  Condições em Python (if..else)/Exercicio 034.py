7f4479bc7679c2d20a14ec2a04152b556e15334e
fe-sts <felipe.rinaldini@gmail.com>
2020-12-25 22:46:58 -0300

    Exercicio 034: if | elif Z float

---
+++ Lista de Exercicios/3.  Condições em Python (if..else)/Exercicio 034.py
@@ -0,0 +1,10 @@
+#Faça um programa que leia o salario de um funcionário
+# Se tiver um salario acima de R$1250, tem 10% de aumento
+# Se for abaixo, tem %15 de aumento
+
+salario = float(input('Entre com o salario: ').strip())
+
+if salario > 1250:
+    print('O novo salario é de {:.2f}'.format(salario * 1.1))
+elif salario <= 1250:
+    print('O novo salario é de {:.2f}'.format(salario * 1.15))
