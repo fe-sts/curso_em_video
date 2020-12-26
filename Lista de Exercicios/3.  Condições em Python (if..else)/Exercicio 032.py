c9cda9435a1b5c11b92c89b19df80fa84f01aa89
fe-sts <felipe.rinaldini@gmail.com>
2020-12-26 00:10:01 -0300

    from datetime import date

--- Lista de Exercicios/3.  Condições em Python (if..else)/Exercicio 032.py
+++ Lista de Exercicios/3.  Condições em Python (if..else)/Exercicio 032.py
@@ -1,8 +1,13 @@
 #Faça um programa que leia um ano qualquer e mostre se ele é bissexto
+from datetime import date
 
 ano = int(input('Digite o ano para saber se é Bissexto: '))
+if ano == 0:
+    ano = date.today().year
 if (ano % 4) == 0:
     if ((ano % 100) != 0) or (ano % 400) == 0:
         print('O ano {} é bissexto!'.format(ano))
+    else:
+        print("O ano {} não é bissexto.".format(ano))
 else:
     print("O ano {} não é bissexto.".format(ano))
