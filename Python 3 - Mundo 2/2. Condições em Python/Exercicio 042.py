cc7d421fed21759075b0a22afbca735d3833c2fa
fe-sts <felipe.rinaldini@gmail.com>
2020-12-29 20:05:34 -0300

    aninhamentos| if|elif|lógica

---
+++ Python 3 - Mundo 2/2. Condições em Python/Exercicio 042.py
@@ -0,0 +1,33 @@
+'''
+Refaça o exercicio 035 dos triângulos. Acrescente o recurso de mostrar que tipo de triângulo será formado:
+- Equilátero: todos os lados iguais
+- Isóceles: 2 lados iguais
+- Escaleno: todos lados diferentes
+'''
+
+#Leia o comprimento de 3 retas e descubra se podem formar um triângulo
+#* o comprimento de uma reta não pode ser maior que a soma do comprimento das outras 2 retas!
+print('=-='*20)
+print('Analisador de Triângulos')
+print('=-='*20)
+r1 = float(input('Entre com o comprimento da reta 1: '))
+r2 = float(input('Entre com o comprimento da reta 2: '))
+r3 = float(input('Entre com o comprimento da reta 3: '))
+
+
+if (r1 + r2) > r3 or (r1 + r3) > r2 or r2 + r3 > r1:
+    print('Sim! É possível formar um triângulo com essas retas!')
+else:
+    print('Não é possível formar um triângulo com essas retas!')
+
+#OUTRO JEITO:
+if r1 < (r2 + r3) or r2 < (r1 + r3) or r3 < (r1 + r2):
+    print('Ok, é possivel formar um triângulo ', end = '')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO')
+    else:
+        print('ISÓCELES')
+else:
+    print('Não é possível formar um triângulo com essas retas!')
