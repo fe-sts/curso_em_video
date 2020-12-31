c4af800b100292d558ccb017eef4798d0d5a0fb6
fe-sts <felipe.rinaldini@gmail.com>
2020-12-31 01:52:27 -0300

    aninhamentos| if|elif|lógica| conversão de bases [binaria, octal, hexa]

--- Python 3 - Mundo 2/2. Condições em Python/Exercicio 037.py
+++ Python 3 - Mundo 2/2. Condições em Python/Exercicio 037.py
@@ -6,13 +6,15 @@ conversão:
 
 num = int(input('Digite um numero inteiro: ').strip())
 base = int(input('Escolha a base para qual quer converter'
-                 '\n 1: binário'
-                 '\n 2: octal'
-                 '\n 3: hexadecimal'
-                 '\n---> ').strip())
+                 '\n [1]: binário'
+                 '\n [2]: octal'
+                 '\n [3]: hexadecimal'
+                 '\n---> '))
 if base == 1:
-    print('Base Binária = \033[1;35;40m {} \033[m'.format(bin(base)))
+    print('Base Binária = \033[1;35;40m {} \033[m'.format(bin(num)[2:]))
 elif base == 2:
-    print('Base Octal = \033[1;31;40m {} \033[m'.format(oct(base)))
+    print('Base Octal = \033[1;31;40m {} \033[m'.format(oct(num)[2:]))
 elif base == 3:
-    print('Base Hexadecimal = \033[1;31;42m {} \033[m'.format(bin(base)))
+    print('Base Hexadecimal = \033[1;31;42m {} \033[m'.format(hex(num)[2:]))
+else:
+    print('Opção inválida.')
