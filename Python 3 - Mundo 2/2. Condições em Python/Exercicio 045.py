4e5a90051255c704488cb3746f17a4b454d1699f
fe-sts <felipe.rinaldini@gmail.com>
2020-12-31 02:44:31 -0300

    aninhamentos| if|elif|lógica| import sleep| randomização de posições

--- Python 3 - Mundo 2/2. Condições em Python/Exercicio 045.py
+++ Python 3 - Mundo 2/2. Condições em Python/Exercicio 045.py
@@ -2,17 +2,18 @@
 Crie um programa que faça o computador jogar Jokenpô contra você.
 '''
 import random
-escolha_jogador = int(input('Escolha:\n'
-                            '[1] Pedra\n'
+from time import sleep
+
+escolha_jogador = int(input('[1] Pedra\n'
                             '[2] Papel\n'
                             '[3] Tesoura\n'
-                            '-->').strip())
+                            'Escolha a sua jogada: ').strip())
 #list_comp = [1,2,3]
 list_comp = random.choice([1,2,3])
 
 if escolha_jogador == 1:
     jog = 'Pedra'
-elif escolha_jogador ==2:
+elif escolha_jogador == 2:
     jog = 'Papel'
 elif escolha_jogador == 3:
     jog = 'Tesoura'
@@ -24,8 +25,16 @@ elif list_comp ==2:
 elif list_comp == 3:
     computador = 'Tesoura'
 
-print('COMPUTADOR ESCOLHEU: {}\n'
-      'VOCÊ ESCOLHEU: {}'.format(computador, jog))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
+sleep(1)
+print('-='*15)
+print('COMPUTADOR JOGOU: {}\n'
+      'VOCÊ JOGOU: {}'.format(computador, jog))
+print('-='*15)
 
 if escolha_jogador == list_comp:
     print('EMPATE')
@@ -41,3 +50,8 @@ elif escolha_jogador == 3 and list_comp == 1:
     print('COMPUTADOR GANHOU!')
 elif escolha_jogador == 3 and list_comp == 2:
     print('VOCÊ GANHOU!')
+
+#OUTRO JEITO DE RANDOMIZAR ESCOLHAS
+itens = ('nenhum', 'Pedra', 'Papel', 'Tesoura')
+computador2 = random.randint(1, 3)
+print('TESTE --> O computador2 escolheu: {}'.format(itens[computador2]))
