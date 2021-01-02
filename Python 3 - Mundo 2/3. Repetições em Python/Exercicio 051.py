7dd669bbeb417b927093a120faeb21582591b955
fe-sts <felipe.rinaldini@gmail.com>
2021-01-02 19:53:11 -0300

    for | range | PA

---
+++ Python 3 - Mundo 2/3. Repetições em Python/Exercicio 051.py
@@ -0,0 +1,11 @@
+'''
+Desenvolva um programa que leia o 1o termo  ea razão de uma PA.
+No final, mostre os 10 primeiros termos dessa progressão.
+'''
+
+termo1 = int(input('Digite o 1o termo da PA: ').strip())
+razao = int(input('Digite a razão da PA: ').strip())
+
+for c in range(1, 11):
+    print('Termo {} de 10: {}'.format(c, termo1))
+    termo1 += razao
