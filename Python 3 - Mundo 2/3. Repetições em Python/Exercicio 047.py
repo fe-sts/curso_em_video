b4573e396e8be2ed95a31fcf2b65413fd424836c
fe-sts <felipe.rinaldini@gmail.com>
2021-01-02 03:00:27 -0300

    import sleep | for | range | lógica

--- Python 3 - Mundo 2/3. Repetições em Python/Exercicio 047.py
+++ Python 3 - Mundo 2/3. Repetições em Python/Exercicio 047.py
@@ -2,7 +2,7 @@
 Faça um programa que mostre todos os numeros pares entre 1 e 50.
 '''
 from time import sleep
-for c in range(1, 50):
+for c in range(1, 51):
     if (c % 2) == 0:
         print('Numero par: {}'.format(c))
-        sleep(0.5)
+        sleep(0.1)
---
+++ Python 3 - Mundo 2/3. Repetições em Python/Exercicio 048.py
@@ -0,0 +1,14 @@
+'''
+Faa um programa que calcule a soma de todos os numeros impares que são multiplos de 3 e que se encontram no
+intervalos entre 1 até 500.
+'''
+
+soma = 0
+cont = 0
+for c in range(1, 501):
+    if (c % 2) == 1:
+        if (c % 3) == 0:
+            soma = soma + c
+            cont += 1
+print('Total da soma: {}'.format(soma))
+print('Foram somados {} números'.format(cont))
