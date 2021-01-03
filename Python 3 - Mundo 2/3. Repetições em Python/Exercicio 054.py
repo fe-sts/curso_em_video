37a6076ee4718bcc04bbf80578d42cc0eb73ecda
fe-sts <felipe.rinaldini@gmail.com>
2021-01-03 01:32:20 -0300

    for | contadores

--- Python 3 - Mundo 2/3. Repetições em Python/Exercicio 054.py
+++ Python 3 - Mundo 2/3. Repetições em Python/Exercicio 054.py
@@ -6,10 +6,10 @@ cont_maior = 0
 cont_menor = 0
 for c in range(1, 8):
     idade = int(input('Digite a idade da {}ª pessoa: '.format(c)).strip())
-    if idade >= 18:
+    if idade >= 21:
         cont_maior += 1
-    elif idade < 18:
+    elif idade < 21:
         cont_menor += 1
 print('='*50)
-print('{} pessoas tem 18 anos ou mais'.format(cont_maior))
-print('{} pessoas tem menos de 18 anos'.format(cont_menor))
+print('{} pessoas tem 21 anos ou mais'.format(cont_maior))
+print('{} pessoas tem menos de 21 anos'.format(cont_menor))
