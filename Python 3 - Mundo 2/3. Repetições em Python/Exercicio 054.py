56fda47a96720c08969c6d3f1060e7557ff54e42
fe-sts <felipe.rinaldini@gmail.com>
2021-01-03 01:28:02 -0300

    for | contadores

---
+++ Python 3 - Mundo 2/3. Repetições em Python/Exercicio 054.py
@@ -0,0 +1,15 @@
+'''
+Crie um programa que leia o ano de nascimento de 7 pessoas.
+No final, mostre quantas pessoas ainda não atingiram a maioridade a quantas já são maiores.
+'''
+cont_maior = 0
+cont_menor = 0
+for c in range(1, 8):
+    idade = int(input('Digite a idade da {}ª pessoa: '.format(c)).strip())
+    if idade >= 18:
+        cont_maior += 1
+    elif idade < 18:
+        cont_menor += 1
+print('='*50)
+print('{} pessoas tem 18 anos ou mais'.format(cont_maior))
+print('{} pessoas tem menos de 18 anos'.format(cont_menor))
