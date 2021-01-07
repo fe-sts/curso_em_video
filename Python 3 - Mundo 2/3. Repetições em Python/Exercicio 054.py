6230631939109e55d9f4892ae0fd7af6f87ea3b6
fe-sts <felipe.rinaldini@gmail.com>
2021-01-07 00:38:34 -0300

    import datetime | ano atual datetime.date.today().year

--- Python 3 - Mundo 2/3. Repetições em Python/Exercicio 054.py
+++ Python 3 - Mundo 2/3. Repetições em Python/Exercicio 054.py
@@ -2,13 +2,18 @@
 Crie um programa que leia o ano de nascimento de 7 pessoas.
 No final, mostre quantas pessoas ainda não atingiram a maioridade a quantas já são maiores.
 '''
+import datetime
 cont_maior = 0
 cont_menor = 0
+ano_atual = datetime.date.today().year
+'''data_atual = datetime.datetime.now()
+ano_atual = int(data_atual.strftime('%Y'))'''#Converte String em Inteiro
+
 for c in range(1, 8):
-    idade = int(input('Digite a idade da {}ª pessoa: '.format(c)).strip())
-    if idade >= 21:
+    ano_nasc = int(input('Digite o ano de nascimento da {}ª pessoa: '.format(c)).strip())
+    if ano_atual - ano_nasc >= 21:
         cont_maior += 1
-    elif idade < 21:
+    elif ano_atual - ano_nasc < 21:
         cont_menor += 1
 print('='*50)
 print('{} pessoas tem 21 anos ou mais'.format(cont_maior))
