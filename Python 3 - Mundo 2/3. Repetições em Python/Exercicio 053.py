84dec53a7e42c3b458b286134c5053a4f4d49ec5
fe-sts <felipe.rinaldini@gmail.com>
2021-01-06 23:25:47 -0300

    if | join | split | lista reversa

--- Python 3 - Mundo 2/3. Repetições em Python/Exercicio 053.py
+++ Python 3 - Mundo 2/3. Repetições em Python/Exercicio 053.py
@@ -1,12 +1,14 @@
 '''
-Desenvolva um programa que leia o 1o termo
+Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+Exemplos de palíndromos:
+APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA.
 '''
 
-palavra = str(input('Digite uma frase para saber se é palíndromo: ').strip())
-pal_sem_espaco = ''.join(palavra.split())
-reversa = pal_sem_espaco[::-1]
+frase = str(input('Digite uma frase para saber se é palíndromo: ').strip().upper())
+frase_sem_espaco = ''.join(frase.split())
+reversa = frase_sem_espaco[::-1]
 print('\033[1:31:40m A palavra invertida fica:\033[m \n{} '.format(reversa))
-if pal_sem_espaco == reversa:
-    print('A frase é um palíndromo')
+if frase_sem_espaco == reversa:
+    print('A frase é um palíndromo!')
 else:
     print('A frase Não é um palíndromo!')
