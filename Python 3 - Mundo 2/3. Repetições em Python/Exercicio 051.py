6c9da41d76786157ff260eb19b90e233a03e4540
fe-sts <felipe.rinaldini@gmail.com>
2021-01-06 23:18:23 -0300

    for | acumulador

--- Python 3 - Mundo 2/3. Repetições em Python/Exercicio 051.py
+++ Python 3 - Mundo 2/3. Repetições em Python/Exercicio 051.py
@@ -5,7 +5,7 @@ No final, mostre os 10 primeiros termos dessa progressão.
 
 termo1 = int(input('Digite o 1o termo da PA: ').strip())
 razao = int(input('Digite a razão da PA: ').strip())
-
+#decimo = termo1 + ((10 - 1) * razao)
 for c in range(1, 11):
     print('Termo {} de 10: {}'.format(c, termo1))
     termo1 += razao
