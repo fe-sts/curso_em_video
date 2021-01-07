10ecbfeb700b9ecdb9b743fcd5a01269e6244a44
fe-sts <felipe.rinaldini@gmail.com>
2021-01-07 00:45:44 -0300

    if | logica maior e menor peso

--- Python 3 - Mundo 2/3. Repetições em Python/Exercicio 055.py
+++ Python 3 - Mundo 2/3. Repetições em Python/Exercicio 055.py
@@ -3,13 +3,17 @@ Faça um programa que leia o peso de 5 pessoas.
 No final, mostre qual foi o maior eo menor peso lidos.
 '''
 maior = 0
-menor = 500
+menor = 0
 print('Digite os Pesos:\n', end = '')
 for c in range(1, 6):
     peso = float(input('{}ª pessoa: '.format(c)).strip())
-    if peso > maior:
+    if c == 1:
         maior = peso
-    if peso < menor:
         menor = peso
-print('O maior peso foi: {}'. format(maior))
-print('O menor peso foi: {}'. format(menor))
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso foi: {} kg'. format(maior))
+print('O menor peso foi: {} kg'. format(menor))
