5804ed48e141f29a78a92d3bd4d384ddc81ef7ad
Felipe <felipe.rinaldini@gmail.com>
2021-06-14 23:02:49 -0300

    Listas | pilha | append | pop()

---
+++ Python 3 - Mundo 3/2. Listas/exercicio 083.py
@@ -0,0 +1,21 @@
+'''
+Exercício Python 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. 
+Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
+'''
+expressao = str(input('Digite uma expressão matematica: '))
+pilha = []
+
+for simbolo in expressao:
+    if simbolo == '(':
+        pilha.append('(')
+    elif simbolo == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Expressão é Válida!')
+else:
+    print('Expressão está Errada!')
