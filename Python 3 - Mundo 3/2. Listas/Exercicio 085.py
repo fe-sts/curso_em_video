72adeb4388082d63047ed118e2be55f02fa67616
Felipe <felipe.rinaldini@gmail.com>
2021-06-20 23:50:50 -0300

    Listas dentro de Listas utilizando append()

---
+++ Python 3 - Mundo 3/2. Listas/Exercicio 085.py
@@ -0,0 +1,15 @@
+'''
+Exercício Python 085: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em 
+uma lista única que mantenha separados os valores pares e ímpares. 
+No final, mostre os valores pares e ímpares em ordem crescente.
+'''
+numeros = [[], []]
+
+for i in range(0, 7):
+    valor = int(input('Digite um numero: '))
+    if valor % 2 == 0:
+        numeros[0].append(valor)
+    else: 
+        numeros[1].append(valor)
+print(f'Numeros pares: {numeros[0]}')
+print(f'Numeros ímpares: {numeros[1]}')
