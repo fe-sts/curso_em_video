59836a4e9fabe1ec6cc1a537f2e1e3e80125fb6b
Felipe <felipe.rinaldini@gmail.com>
2021-06-25 01:04:08 -0300

    Sorteio Megasena | Matriz | enumerate
    Sorteio Megasena | Matriz | while | enumerate

--- Python 3 - Mundo 3/2. Listas/Exercicio 088.py
+++ Python 3 - Mundo 3/2. Listas/Exercicio 088.py
@@ -3,25 +3,57 @@ Exercício Python 088: Faça um programa que ajude um jogador da MEGA SENA a cri
 O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, 
 cadastrando tudo em uma lista composta.
 '''
+
+'''
+# RESOLUÇÃO 1
+
 from random import randint
 
 num_jogos = 0
 num = 0
 lista_jogo = []
 lista_conjuto = []
-'''
-while True:
-    num_jogos = int(input('Quantos jogos deseja fazer para Megasena? ').strip())
-    for megasena in range(num_jogos):
-'''
-num_jogos = int(input('Quantos jogos deseja fazer para Megasena? ').strip())
- 
-for j in range(num_jogos):
-    for i in range(6):
-        num = randint(0, 60)
-        lista_jogo.append(num)
-    lista_conjuto.append(lista_conjuto)
 
-for l in range(len(lista_conjuto)):
-    print(lista_conjuto[l][l])
-#print(lista_jogo)
+num_jogos = int(input('Quantos jogos deseja fazer para Megasena? ').strip())
+cont = 0
+
+for i in range(num_jogos):
+    for l in range(6):
+        num = randint(1, 60)
+        if num not in lista_jogo:
+            lista_jogo.append(num)
+        else:
+            num = randint(0, 60)
+            lista_jogo.append(num)
+    lista_conjuto.append(lista_jogo[:])
+    lista_jogo.clear()
+print(lista_conjuto)
+'''
+
+#RESOLUÇÃO 2
+
+from random import randint
+
+num_jogos = 0
+num = 0
+lista_jogo = []
+lista_conjuto = []
+
+num_jogos = int(input('Quantos jogos deseja fazer para Megasena? ').strip())
+total = 1
+while total <= num_jogos:
+    cont = 0
+    total += 1
+    while True:
+        num = randint(1, 60)
+        if num not in lista_jogo:
+            lista_jogo.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista_jogo.sort()
+    lista_conjuto.append(lista_jogo[:])
+    lista_jogo.clear()
+    
+for chave, valor in enumerate(lista_conjuto):
+    print(f'Jogo: {chave+1}: {valor}')
