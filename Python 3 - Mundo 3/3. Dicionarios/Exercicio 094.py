fc9d90c3c99cb889cb6b249bfe6be51df206f70c
Felipe <felipe.rinaldini@gmail.com>
2021-07-26 02:08:02 -0300

    Completo
    Listas, Dicionario, For, While

---
+++ Python 3 - Mundo 3/3. Dicionarios/Exercicio 094.py
@@ -0,0 +1,33 @@
+'''
+Exercício Python 094: 
+Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um 
+dicionário e todos os dicionários em uma lista. No final, mostre: 
+A) Quantas pessoas foram cadastradas 
+B) A média de idade 
+C) Uma lista com as mulheres
+ D) Uma lista de pessoas com idade acima da média'''
+
+
+conjunto = dict()
+lista = list()
+soma_idade = 0
+
+while True:
+    conjunto["nome"] = input("Nome: ")
+    while True:
+        conjunto["sexo"] = input("Sexo [M/F]: ")
+        if conjunto["sexo"] in "MmFf":
+            break
+        else:
+            print("ERRO: Digite M ou F")
+    conjunto["idade"] = int(input("Idade: "))
+    lista.append(conjunto.copy())
+    cont = input("Quer continuar? [S/N] ")
+    if cont in "Nn":
+        break
+    
+print(lista)
+print(f'Ao todo temos {len(lista)} pessoas cadastradas.')
+
+for indice, valor in enumerate(lista):
+    print(indice, valor)
