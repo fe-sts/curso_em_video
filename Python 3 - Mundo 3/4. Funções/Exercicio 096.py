24a2427ddebb89005472d3d19764cb7778fca328
Felipe <felipe.rinaldini@gmail.com>
2021-08-05 22:22:06 -0300

    função basica com return

--- Python 3 - Mundo 3/4. Funções/Exercicio 096.py
+++ Python 3 - Mundo 3/4. Funções/Exercicio 096.py
@@ -3,3 +3,18 @@ Exercício Python 096: Faça um programa que tenha uma função chamada área(),
 terreno retangular (largura e comprimento) e mostre a área do terreno.
 
 '''
+largura = 0
+comprimento = 0
+
+
+def area(largura, comprimento):
+    area_c = largura * comprimento
+    return area_c
+
+print(' CONTROLE DE TERRENOS')
+print('-'*20)
+largura = float(input("Digite a largura: ").strip())
+comprimento = float(input("Digite o comprimento: ").strip())
+
+area_calc = area(largura, comprimento)
+print(f'A área calculada é de: {area_calc}m².')
